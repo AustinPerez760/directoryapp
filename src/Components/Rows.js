713653ad47af8433b56cf21b78c853039fc3f2f3
Austin Perez <austin.perez760@gmail.com>
2021-03-08 02:32:57 -0800

    created row component for employee info

---
+++ src/Components/Rows.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Rows(props) {
+  return props.employess.map((item, index) => {
+    return (
+      <tr key={index}>
+        <th scope="row">{item.id.value}</th>
+        <td>
+          <img src={item.picture.thumbnail} />
+        </td>
+        <td>
+          {item.name.first} {item.name.last}
+        </td>
+        <td>{item.cell}</td>
+        <td>{item.email}</td>
+        <td>{item.location.city}</td>
+      </tr>
+    );
+  });
+}
+
+export default Rows;
